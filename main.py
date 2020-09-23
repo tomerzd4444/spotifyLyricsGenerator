cf01b7239aa75645c857f8548f5a9194ab35b87a
tomerzd4444 <50952695+tomerzd4444@users.noreply.github.com>
2020-09-23 17:04:04 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import spotipy
+from Song import Song
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 import lyricsgenius
-genius = lyricsgenius.Genius("my_client_access_token_here")
+import time
+
+delay = 5  # seconds
 load_dotenv(dotenv_path="client.env")
 client_id = os.getenv('id')
 client_secret = os.getenv('secret')
-
+client_genius_secret = os.getenv('genius_secret')
+genius = lyricsgenius.Genius(client_genius_secret)
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri="https://localhost:8080/callback/",
                                                scope="user-library-read user-read-currently-playing"))
 
-results = sp.current_user_playing_track()
-item = results['item']
-albumData = item['album']
-artist = albumData['artists'][0]['name']
-albumName = albumData['name']
-songName = item['name']
-print(f'band name: {artist}\nalbum name: {albumName}\nsong name: {songName}')
-song = genius.search_song(songName, artist)
-print(song.lyrics)
+
+def getSong(spotifyAccount: spotipy.Spotify):
+    results = spotifyAccount.current_user_playing_track()
+    item = results['item']
+    albumData = item['album']
+    artist = albumData['artists'][0]['name']
+    albumName = albumData['name']
+    songName = item['name']
+    return Song(artist, albumName, songName, genius, results['progress_ms'], item['duration_ms'])
+
+
+while True:
+    song1 = getSong(sp)
+    print(song1.current_ms, song1.total_ms - 10, song1.current_ms >= song1.total_ms - 10)
+    if song1.current_ms >= song1.total_ms - 10 or song1.current_ms <= 1000:
+        time.sleep(1)
+        song1 = getSong(sp)
+        print(song1.getLyrics())
