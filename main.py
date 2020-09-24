21731718df1a4d214f0565f2586e13d0f9d62569
tomerzd4444 <50952695+tomerzd4444@users.noreply.github.com>
2020-09-24 16:58:30 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,51 @@ from dotenv import load_dotenv
 import os
 import lyricsgenius
 import time
+from tkinter import *
+import tkinter.font as tkFont
+root = Tk()
+var = StringVar()
+fontStyle = tkFont.Font(size=10)
+label = Label(root, textvariable=var, relief=RAISED, font=fontStyle)
+delay = 5  # seconds
+load_dotenv(dotenv_path="client.env")
+client_id = os.getenv('id')
+client_secret = os.getenv('secret')
+client_genius_secret = os.getenv('genius_secret')
+genius = lyricsgenius.Genius(client_genius_secret)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri="https://localhost:8080/callback/",
+                                               scope="user-library-read user-read-currently-playing"))
+
+
+def getSong(spotifyAccount: spotipy.Spotify):
+    results = spotifyAccount.current_user_playing_track()
+    item = results['item']
+    albumData = item['album']
+    artist = albumData['artists'][0]['name']
+    albumName = albumData['name']
+    songName = item['name']
+    return Song(artist, albumName, songName, genius, results['progress_ms'], item['duration_ms'])
+
+
+while True:
+    song1 = getSong(sp)
+    if song1.current_ms <= 2000:
+        time.sleep(1)
+        song1 = getSong(sp)
+        var.set(song1.getLyrics())
+        label.pack()
+    root.update_idletasks()
+    root.update()
+=======
+import spotipy
+from Song import Song
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+import lyricsgenius
+import time
 
 delay = 5  # seconds
 load_dotenv(dotenv_path="client.env")
