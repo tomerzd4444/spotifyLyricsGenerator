6c492c43a06c8e7ffc3fbdb15874f011ef0cc238
tomerzd4444 <50952695+tomerzd4444@users.noreply.github.com>
2020-10-08 15:34:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import lyricsgenius
 import time
 from tkinter import *
 import tkinter.font as tkFont
+from requests.exceptions import ConnectionError
 root = Tk()
 var = StringVar()
 fontStyle = tkFont.Font(size=10)
@@ -16,11 +17,22 @@ load_dotenv(dotenv_path="client.env")
 client_id = os.getenv('id')
 client_secret = os.getenv('secret')
 client_genius_secret = os.getenv('genius_secret')
-genius = lyricsgenius.Genius(client_genius_secret)
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri="https://localhost:8080/callback/",
                                                scope="user-library-read user-read-currently-playing"))
+
+
+def changeText(text: str):
+    newText = text.split('\n')
+    print(newText)
+    newText2 = ''
+    for count, i in enumerate(newText):
+        newText2 += i + ' '
+        if count % 2 != 0:
+            newText2 += '\n'
+        print(count, count % 2)
+    return newText2
 
 
 def getSong(spotifyAccount: spotipy.Spotify):
@@ -30,53 +42,23 @@ def getSong(spotifyAccount: spotipy.Spotify):
     artist = albumData['artists'][0]['name']
     albumName = albumData['name']
     songName = item['name']
+    genius = lyricsgenius.Genius(client_genius_secret)
     return Song(artist, albumName, songName, genius, results['progress_ms'], item['duration_ms'])
 
 
 while True:
     song1 = getSong(sp)
+    print(song1.current_ms, song1.current_ms <= 2000)
     if song1.current_ms <= 2000:
-        time.sleep(1)
         song1 = getSong(sp)
-        var.set(song1.getLyrics())
+        lyrics = ''
+        while lyrics == '':
+            try:
+                lyrics = song1.getLyrics()
+            except ConnectionError as e:
+                print(e)
+                time.sleep(0.5)
+        var.set(changeText(lyrics))
         label.pack()
     root.update_idletasks()
     root.update()
-=======
-import spotipy
-from Song import Song
-from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-import os
-import lyricsgenius
-import time
-
-delay = 5  # seconds
-load_dotenv(dotenv_path="client.env")
-client_id = os.getenv('id')
-client_secret = os.getenv('secret')
-client_genius_secret = os.getenv('genius_secret')
-genius = lyricsgenius.Genius(client_genius_secret)
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri="https://localhost:8080/callback/",
-                                               scope="user-library-read user-read-currently-playing"))
-
-
-def getSong(spotifyAccount: spotipy.Spotify):
-    results = spotifyAccount.current_user_playing_track()
-    item = results['item']
-    albumData = item['album']
-    artist = albumData['artists'][0]['name']
-    albumName = albumData['name']
-    songName = item['name']
-    return Song(artist, albumName, songName, genius, results['progress_ms'], item['duration_ms'])
-
-
-while True:
-    song1 = getSong(sp)
-    print(song1.current_ms, song1.total_ms - 10, song1.current_ms >= song1.total_ms - 10)
-    if song1.current_ms >= song1.total_ms - 10 or song1.current_ms <= 1000:
-        time.sleep(1)
-        song1 = getSong(sp)
-        print(song1.getLyrics())
